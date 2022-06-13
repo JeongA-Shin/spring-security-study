e6c865ef92a3da5c18f61d2b99a90c767809a4f9
신정아 <jeonga@khu.ac.kr>
2022-06-13 16:25:33 +0900

    project inint, add README.md

---
+++ src/test/java/group/securitystudy/SecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package group.securitystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
